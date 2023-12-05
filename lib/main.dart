3879fa1065b8c67fbd93837d3fa36b110dafbf31
ORCA <ORCA@iMac-Anton.local>
2023-12-06 03:05:48 +0500

    üpdate version

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:get/get_core/get_core.dart';
+import 'package:get/instance_manager.dart';
+import 'package:hive/hive.dart';
 import 'package:modern_vpn_project/src/DI/di_container.dart';
 import 'package:modern_vpn_project/src/router.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -10,6 +15,9 @@ void main() async {
 
   final shared = await SharedPreferences.getInstance();
   DI.init(shared);
+  var path = Directory.current.path;
+  Hive.init(path);
+  Get.lazyPut<bool>(()=>true);
   runApp(VPN());
 }
 
